ae9cf48b4b39c4058c4cc35b9d0a4061cf9aebac
frort <87010782+kienn-HCl@users.noreply.github.com>
2023-03-24 15:13:24 +0900

    change main.go

--- main.go
+++ main.go
@@ -10,92 +10,111 @@ import (
 	pdfcpu "github.com/pdfcpu/pdfcpu/pkg/api"
 )
 
-type PdfData struct {
-    Title       string `csv:"Title"`
-    Author      string `csv:"Author"`
+type ReportData struct {
     PageCount   int    `csv:"PageCount"`
+    Author      string `csv:"Author"`
+    Title       string `csv:"Title"`
     Filename    string `csv:"Filename"`
 }
 
-func genCsv() error {
+type ReportDatas []ReportData
+
+func genCsv() error { // ディレクトリにある".pdf"で終わるファイルを読み取り"repsrtData.csv"を作成する。
     files, err := filepath.Glob("./*.pdf")
     if err != nil {
         return err
     }
 
-    pdfs := make([]PdfData, 0, len(files))
+    reports := make(ReportDatas, 0, len(files))
 
     for _, file := range files {
         // aaa, _ := api.InfoFile(file, nil, nil)
         // println(file)
         // println(aaa[1], aaa[5], aaa[6])
-        con, err := pdfcpu.ReadContextFile(file)
+        ctx, err := pdfcpu.ReadContextFile(file)
         if err != nil {
             return err
         }
-        pdfs = append(pdfs, PdfData{con.Title, con.Author, con.PageCount, file})
+        reports = append(reports, ReportData{ctx.PageCount, ctx.Author, ctx.Title, file})
     }
 
-    f, err := os.Create("pdfData.csv")
+    f, err := os.Create("reportData.csv")
     if err != nil {
         return err
     }
     defer f.Close()
 
-    err = gocsv.MarshalFile(&pdfs, f)
+    err = gocsv.MarshalFile(&reports, f)
     if err != nil {
         return err
     }
     return nil
 }
 
-func readCsv() ([]PdfData, error) {
-    f, err := os.Open("pdfData.csv")
-    if err != nil {
-        return nil, err
-    }
-    defer f.Close()
-
-    var pdfs []PdfData
-    err = gocsv.UnmarshalFile(f, &pdfs)
-    if err != nil {
-        return nil, err
-    }
-
-    return pdfs, nil
-}
-
-func unitePdf(pdfs []PdfData) error {
-    pdfFilename := make([]string, 0, len(pdfs))
-    for _, pdf := range pdfs {
-        pdfFilename = append(pdfFilename, pdf.Filename)
-    }
-
-    err := os.MkdirAll("Unite", 0755)
+func initReportBinding() error {
+    genCsv()
+    err := os.MkdirAll("./Hyougi", 0755)
     if err != nil {
         return err
     }
-    err = pdfcpu.MergeCreateFile(pdfFilename, "Unite/united.pdf", nil)
+    err = os.MkdirAll("./Mokuzi", 0755)
+    if err != nil {
+        return err
+    }
+    err = os.MkdirAll("./UraByougi", 0755)
     if err != nil {
         return err
     }
     return nil
 }
 
-func addPagenum(startPage int) error {
+func newReportDatas() (ReportDatas, error) { // ディレクトリにある"reportData.csv"を読み込みreportDatasを作成する
+    f, err := os.Open("reportData.csv")
+    if err != nil {
+        return nil, err
+    }
+    defer f.Close()
+
+    var reports ReportDatas
+    err = gocsv.UnmarshalFile(f, &reports)
+    if err != nil {
+        return nil, err
+    }
+
+    return reports, nil
+}
+
+func (r ReportDatas)uniteReport() error { // reportDatasにあるpdfを結合する
+    reportFilenames := make([]string, 0, len(r))
+    for _, report := range r {
+        reportFilenames = append(reportFilenames, report.Filename)
+    }
+
+    err := os.MkdirAll("UniteReport", 0755)
+    if err != nil {
+        return err
+    }
+    err = pdfcpu.MergeCreateFile(reportFilenames, "UniteReport/uniteReport.pdf", nil)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func (r ReportDatas)addPagenum(startPage int) error {
     for i:=0; i< startPage; i++ {
-        err := pdfcpu.InsertPagesFile("Unite/united.pdf", "", []string{"1"}, true, nil)
+        err := pdfcpu.InsertPagesFile("UniteReport/uniteReport.pdf", "", []string{"1"}, true, nil)
         if err != nil {
             return err
         }
     }
 
-    err := pdfcpu.AddTextWatermarksFile("Unite/united.pdf", "", nil, false, "%p / %P", "sc:1.0 abs, points: 12, pos:bc, rot:0, fillc:#000000, ma:10", nil)
+    err := pdfcpu.AddTextWatermarksFile("UniteReport/uniteReport.pdf", "", nil, false, "%p / %P", "sc:1.0 abs, points: 12, pos:bc, rot:0, fillc:#000000, ma:10", nil)
     if err != nil {
         return err
     }
 
-    err = pdfcpu.RemovePagesFile("Unite/united.pdf", "", []string{fmt.Sprintf("1-%d", startPage)}, nil)
+    err = pdfcpu.RemovePagesFile("UniteReport/uniteReport.pdf", "", []string{fmt.Sprintf("1-%d", startPage)}, nil)
     if err != nil {
         return err
     }
@@ -108,22 +127,22 @@ func main () {
     //     log.Fatal(err)
     // }
 
-    pdfs, err := readCsv()
+    reports, err := newReportDatas()
     if err != nil {
         log.Fatal(err)
     }
 
-    //for _, pdf := range pdfs {
+    //for _, pdf := range reports {
     //    fmt.Println(pdf.Filename)
     //}
-    pdfNum := len(pdfs)
+    reportsNum := len(reports)
 
-    err = unitePdf(pdfs)
+    err = reports.uniteReport()
     if err != nil {
         log.Fatal(err)
     }
 
-    err = addPagenum(2 + pdfNum/10)
+    err = reports.addPagenum(2 + reportsNum/10)
     if err != nil {
         log.Fatal(err)
     }
